3cb1f0995441db92a43e36cb0cf90c415110529b
Saska Karsi <saska.karsi@gmail.com>
2020-03-28 15:27:34 +0200

    Base64 decode connstr

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(Buffer.from(process.env.MONGODB_URL, 'base64').toString('utf-8'), {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
